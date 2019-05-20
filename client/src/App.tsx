2d9e792f4fc947569d125f26f85b9db9d3b7eec4
Christian Gärtner <chrisdagardner@gmail.com>
2019-05-20 11:44:04 +0200

    added scaffold for react client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          DMX Controller
+        </p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
